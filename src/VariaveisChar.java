7a8a3279cb7cdeeca2f5d91299f86de611641f08
Heberth Santos <86444094+Alyah12@users.noreply.github.com>
2023-09-29 23:44:24 -0300

    Att

---
+++ src/VariaveisChar.java
@@ -0,0 +1,11 @@
+public class VariaveisChar {
+    public static void main(String[] args) {
+
+        char o =  'o';
+        char i = 'i';
+        char interrogacao = 0x00E1; // Valor = '?'
+
+
+        System.out.println(""+o+i+interrogacao);
+    }
+}
---
+++ src/VariaveisFloat.java
@@ -0,0 +1,11 @@
+public class VariaveisFloat {
+    public static void main(String[] args) {
+
+        double valorPassagem = 2.90;
+
+        float valorTomate = 1.25f;
+
+        System.out.println("Valor da passagem é " + valorPassagem);
+        System.out.println("Valor do tomate hoje é " + valorTomate);
+    }
+}
